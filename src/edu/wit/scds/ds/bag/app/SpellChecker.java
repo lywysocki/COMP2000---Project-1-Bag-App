8041276a2626ac4682389ace6febd364908ff1ba
lywysocki <lywysocki@gmail.com>
2022-09-26 10:37:48 -0400

    SpellChecker initial

---
+++ src/edu/wit/scds/ds/bag/app/SpellChecker.java
@@ -0,0 +1,16 @@
+
+package edu.wit.scds.ds.bag.app;
+
+
+/**
+ * 
+ * 
+ * @author wysockil
+ * @version 1.0.0 2022-09-26 Initial implementation
+ *
+ */
+public class SpellChecker
+    {
+
+    }
+   // end class SpellChecker
