11dfe119d0425629b2a7d4e9256270c356bbe5f7
lywysocki <lywysocki@gmail.com>
2022-09-30 11:30:57 -0400

    add class

--- src/edu/wit/scds/ds/bag/app/Dictionary.java
+++ src/edu/wit/scds/ds/bag/app/Dictionary.java
@@ -4,6 +4,16 @@ package edu.wit.scds.ds.bag.app ;
 import edu.wit.scds.ds.bag.adt.ResizableArrayBag ;
 
 /**
+ * dictuion ary = loading itself & word lopokup
+ * 
+ * spellchecker = overriding logic
+ * 
+ * mispelled words class = only word apears in bag once
+ * 
+ * make try again, check case, check punctuation, (who calls it) 
+ */
+
+/**
  * 
  * 
  * @author wysockil
@@ -14,3 +24,4 @@ public class Dictionary
     {
 
     }   // end class Dictionary
+
---
+++ src/edu/wit/scds/ds/bag/app/MisspelledWords.java
@@ -0,0 +1,15 @@
+
+package edu.wit.scds.ds.bag.app ;
+
+import edu.wit.scds.ds.bag.adt.ResizableArrayBag ;
+/**
+ * 
+ * 
+ * @author wysockil
+ * @version 1.0.0 2022-09-30 Initial implementation
+ *
+ */
+public class MisspelledWords
+    {
+
+    }   // end class MisspelledWords
